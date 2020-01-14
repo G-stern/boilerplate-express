fa51cd363cdf1194a3212dd86f4b820e1652af36
gs11004175 <gs11004175@academic.local>
2020-01-14 11:48:20 -0800

    first working Express server

--- myApp.js
+++ myApp.js
@@ -9,12 +9,12 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-console.log('Hello World!');
+//console.log('Hello World!');
 
 /** 2) A first working Express Server */
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+    res.send('Hello Express');
+})
 
 /** 3) Serve an HTML file */
 
